1762a0e11e6f3a2bb99cc8cc5f51e5c59d1cccde
Jarrod Jackson <github@thespinlight.com>
2016-07-18 18:31:38 -0600

    Changes for compatibility with AWS API Gateway Domain Name certificate checking.

--- self_signed_domain_cert.tf
+++ self_signed_domain_cert.tf
@@ -1,11 +1,16 @@
 resource "tls_private_key" "testCAKey" {
-		algorithm = "ECDSA"
-		ecdsa_curve = "P384"
+		algorithm = "RSA"
+		rsa_bits = "2048"
+}
+
+resource "tls_private_key" "testDomainKey" {
+		algorithm = "RSA"
+		rsa_bits = "2048"
 }
 
 resource "tls_self_signed_cert" "testCACert" {
 	subject {
-		common_name = "example.com"
+		common_name = "test-CA-cert.com"
 		organization = "Example, Inc"
 		organizational_unit = "Department of Terraform Testing"
 		street_address = ["5879 Cotton Link"]
@@ -16,33 +21,39 @@ resource "tls_self_signed_cert" "testCACert" {
 		serial_number = "2"
 	}
 	# about 68 years
-	validity_period_hours = 596520
+	validity_period_hours = 87600
 	allowed_uses = [
 		"cert_signing",
+		"server_auth",
+		"any_extended",
 	]
-	key_algorithm = "ECDSA"
+	key_algorithm = "${tls_private_key.testCAKey.algorithm}"
 	private_key_pem = "${tls_private_key.testCAKey.private_key_pem}"
 	is_ca_certificate = true
 }
 
 resource "tls_cert_request" "testCertRequest" {
-		key_algorithm = "ECDSA"
-		private_key_pem = "${tls_private_key.testCAKey.private_key_pem}"
+		key_algorithm = "${tls_private_key.testDomainKey.algorithm}"
+		private_key_pem = "${tls_private_key.testDomainKey.private_key_pem}"
 
-    subject {
-        common_name = "example.com"
-        organization = "Example, Inc"
+		subject {
+			common_name = "${var.commonName}"
+			organization = "Example, Inc"
     }
 }
 
 resource "tls_locally_signed_cert" "testDomainCert" {
-    cert_request_pem = "${tls_cert_request.testCertRequest.cert_request_pem}"
-		ca_key_algorithm = "ECDSA"
+		cert_request_pem = "${tls_cert_request.testCertRequest.cert_request_pem}"
+		ca_key_algorithm = "${tls_private_key.testCAKey.algorithm}"
 		ca_private_key_pem = "${tls_private_key.testCAKey.private_key_pem}"
 		ca_cert_pem = "${tls_self_signed_cert.testCACert.cert_pem}"
 		# about 68 years
-		validity_period_hours = 596520
+		validity_period_hours = 87600
 		allowed_uses = [
 			"server_auth",
+			"key_encipherment",
+			"digital_signature",
+			"non_repudiation",
+			"any_extended",
     ]
 }
--- vars.tf
+++ vars.tf
@@ -1,13 +1,15 @@
+variable "commonName" {}
+
 output "certificate_body" {
 	value = "${tls_locally_signed_cert.testDomainCert.cert_pem}"
 }
 
 output "certificate_chain" {
-	value = "${format("%s%s",
-		tls_locally_signed_cert.testDomainCert.cert_pem,
-		tls_self_signed_cert.testCACert.cert_pem)}"
+	value = "${format("%s",
+		tls_self_signed_cert.testCACert.cert_pem
+	)}"
 }
 
 output "certificate_private_key" {
-	value = "${tls_private_key.testCAKey.private_key_pem}"
+	value = "${tls_private_key.testDomainKey.private_key_pem}"
 }
